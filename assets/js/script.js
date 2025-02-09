39069d66be89c0b25a51db9fdfaecb3dba550294
sidneylino <sidneymartinslino@gmail.com>
2025-02-09 19:20:24 -0300

    init_project

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('show');
+}
+
+
+
+
+
+function activeInScrollElement() {
+    document.querySelectorAll('.slide-right, .slide-left').forEach((el) => {
+        if (el.getBoundingClientRect().top < window.innerHeight - 150) {
+            el.classList.add('finish');  
+        } else {
+            el.classList.remove('finish')
+        }
+    });
+}
+
+window.addEventListener('scroll', activeInScrollElement);
+
+
