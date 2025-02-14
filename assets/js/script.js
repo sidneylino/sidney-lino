87c9decb43a42c1fda1006bcdacdd50afb416b91
sidneylino <sidneymartinslino@gmail.com>
2025-02-14 09:35:57 -0300

    add_dark_theme_button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
+// show and hide navbar on mobile
 function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('show');
 }
 
+// slide effect
 function activeInScrollElement() {
     document.querySelectorAll('.slide-right, .slide-left').forEach((el) => {
         if (el.getBoundingClientRect().top < window.innerHeight - 100) {
@@ -12,12 +14,23 @@ function activeInScrollElement() {
         }
     });
 }
-
 window.addEventListener('scroll', activeInScrollElement);
 
+// slide effect and zoom immediately
 document.querySelector('.scale-zoom').classList.add('active');
 
 document.querySelectorAll('.slide-bottom-start, .slide-right-start, .slide-left-start').forEach(el => {
     el.classList.add('finish');
 });
+
+//change theme button
+
+const themeButton = document.querySelector('.theme');
+
+function changeTheme() {
+    document.documentElement.classList.toggle('dark');
+    themeButton.classList.toggle('button-dark');   
+};
+
+document.querySelector('.theme').addEventListener('click', changeTheme);
 
